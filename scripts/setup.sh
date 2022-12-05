2e6053b5bb79dc1fcbfaf57fce652abeffac4cdb
David Tan <davidtan@thoughtworks.com>
2022-12-06 10:52:09 +1100

    Fix typo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,4 @@ set -e
 poetry install --no-root
 
 # download spacy language model if it's not already downloaded.
-[ -f /usr/local/lib/python3.10/site-packages/en_core_web_lg/__init__.py ] || python -m spacy download en_core_web_sm
+[ -f /usr/local/lib/python3.10/site-packages/en_core_web_lg/__init__.py ] || python -m spacy download en_core_web_lg
